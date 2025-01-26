d1e698a73a9c2823d3119f4b541d17b9d746a6f9
vinod-kumar1 <vinu22149@gmail.com>
2025-01-27 04:49:43 +0530

    first push

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./app";
+
+let root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<App />);
